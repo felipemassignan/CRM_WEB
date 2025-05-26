5d363425d2e78247a1fc472036a24fb95e1abd8c
Felipe Massignan <114188078+felipemassignan@users.noreply.github.com>
2025-05-26 15:53:29 -0300

    Delete src directory

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.models.lead import Lead
-from src.models.interaction import Interaction
-from src.models.template import Template
-from src.models.reminder import Reminder
-from src.models.user import User
-
-# Este arquivo importa todos os modelos para facilitar o acesso
--- src/models/db.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Instância central do SQLAlchemy para ser usada em todos os modelos
-db = SQLAlchemy()
